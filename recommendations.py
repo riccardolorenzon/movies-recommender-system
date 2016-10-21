f2ee8806496b7196c1fe9fb4467eb6adb6319d01
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2016-10-21 15:20:01 +0200

    Load movielens db function

--- recommendations.py
+++ recommendations.py
@@ -123,3 +123,17 @@ def calculateSimilarItems(prefs,n=10):
         scores=topMatches(itemPrefs,item,n=n,similarity=sim_distance)
         result[item]=scores
     return result
+
+def loadMovieLens(path='/data/movielens'):
+    # Get movie titles
+    movies={}
+    for line in open(path+'/u.item'):
+        (id,title)=line.split('|')[0:2]
+        movies[id]=title
+    # Load data
+    prefs={}
+    for line in open(path+'/u.data'):
+        (user,movieid,rating,ts)=line.split('\t')
+        prefs.setdefault(user,{})
+        prefs[user][movies[movieid]]=float(rating)
+return prefs
